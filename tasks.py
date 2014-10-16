513bd0937bad1dccc8742c7da7b0ae0b58216d3c
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-10-16 22:41:00 +0200

    more first things

--- tasks.py
+++ tasks.py
@@ -1,11 +1,34 @@
+from invoke import Collection, task, run as base_run
+from functools import partial
 
-from invoke import task, run
+run = partial(base_run, echo=True)
 
 
-@task()
+@task
 def fetch_deps():
     run('mkdir -p static/css')
     run('wget https://raw.githubusercontent.com/'
         'picnicss/picnic/master/releases/latest.css -O static/css/picnic.css')
 
 
+@task(default=True)
+def refresh_env():
+    run('pip install -U -r requirements.txt')
+
+
+@task
+def check():
+    run('flake8 --max-complexity 8 *.py mybox testing')
+
+
+@task
+def test():
+    run('py.test')
+
+
+ns = Collection(
+    fetch_deps,
+    refresh_env,
+    check,
+    test,
+)
