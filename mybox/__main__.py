6903c52a78d1ac840492a9956682d2dfc36a0e17
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-11-15 22:23:15 +0100

    suggested code move

--- mybox/__main__.py
+++ mybox/__main__.py
@@ -2,12 +2,12 @@ import morepath
 from werkzeug import serving
 
 import mybox
-
+from mybox.app import App
 
 #XXX: evil
 if __name__ == '__main__':
     config = morepath.setup()
-    config.scan(mybox)
+    config.scan(mybox.app)
     config.commit()
-    serving.run_simple('localhost', 5000, mybox.App(),
+    serving.run_simple('localhost', 5000, App(),
                        use_debugger=True, use_reloader=True)
